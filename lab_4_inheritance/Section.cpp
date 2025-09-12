d29882b8e9e703c5e7f42d13b674ce2456abb65f
Ni_Whale_0s <tikhonov-na2003@mail.ru>
2025-09-12 12:18:12 +0700

    creating arrays of shapes

--- lab_4_inheritance/Section.cpp
+++ lab_4_inheritance/Section.cpp
@@ -22,9 +22,9 @@ void Section::setAngleLine(float angle_size)
 }
 
 void Section::draw(sf::RenderWindow& window) {
-	sf::VertexArray line(sf::PrimitiveType::Lines, 2);
-	line[0].position = sf::Vector2f(x, y);
-	line[1].position = sf::Vector2f(x - size * cos(angle_line), y - size-sin(angle_line));
+	sf::VertexArray line(sf::PrimitiveType::LineStrip, 2);
+	line[0].position = sf::Vector2f(x - size, y - size);
+	line[1].position = sf::Vector2f(x,y);
 	line[0].color = color;
 	line[1].color = color;
 
--- lab_4_inheritance/lab_4_inheritance.cpp
+++ lab_4_inheritance/lab_4_inheritance.cpp
@@ -9,6 +9,7 @@
 #include "Section.h"
 #include "Triangle.h"
 #include "Ellipse.h"
+#include <vector>
 
 using namespace std;
 
@@ -26,58 +27,75 @@ int main()
     sf::RenderWindow window(sf::VideoMode({ WIDTH, HEIGHT }), "Oop labs");
     window.setFramerateLimit(60);
 
-    float x = static_cast<float>(rand() % WIDTH);
-    float y = static_cast<float>(rand() % HEIGHT);
-    float speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
-    float angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    float size = 1.0f + static_cast<float>(rand() % 40) / 10.0f;
-    tPoint point(x, y, speed, setColorCircle(), size, angle);
+    vector<tPoint> points;
+    vector<Section> lines;
+    vector<Circle> circles;
+    vector<Ellipse> ellipses;
+    vector<Rectagle> rectangles;
+    vector<Rhomb> rhombs;
+    vector<Triangle> triangles;
 
-    x = static_cast<float>(rand() % WIDTH);
-    y = static_cast<float>(rand() % HEIGHT);
-    speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
-    angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    size = 1.0f + static_cast<float>(rand() % 40) / 1.0f;
-    float angle_line = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    Section line(x, y, speed, setColorCircle(), angle, size, angle_line);
+    for (int i = 0; i < 5; i++) {
 
-    x = static_cast<float>(rand() % WIDTH);
-    y = static_cast<float>(rand() % HEIGHT);
-    speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
-    angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    float radius = 1.0f + static_cast<float>(rand() % 60) / 1.0f;
-    float outline = 1.0f + static_cast<float>(rand() % 40) / 10.0f;
-    Circle circle(x, y, speed, setColorCircle(), angle, radius, outline);
+        float x = static_cast<float>(rand() % WIDTH);
+        float y = static_cast<float>(rand() % HEIGHT);
+        float speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
+        float angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        float size = 1.0f + static_cast<float>(rand() % 40) / 10.0f;
+        tPoint point(x, y, speed, setColorCircle(), size, angle);
+        points.push_back(point);
 
-    x = static_cast<float>(rand() % WIDTH);
-    y = static_cast<float>(rand() % HEIGHT);
-    speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
-    angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    radius = 1.0f + static_cast<float>(rand() % 260) / 1.0f;
-    Ellipse ellipse(x, y, speed, setColorCircle(), angle, radius);
+        x = static_cast<float>(rand() % WIDTH);
+        y = static_cast<float>(rand() % HEIGHT);
+        speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
+        angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        size = 1.0f + static_cast<float>(rand() % 40) / 1.0f;
+        float angle_line = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        Section line(x, y, speed, setColorCircle(), angle, size, angle_line);
+        lines.push_back(line);
 
-    x = static_cast<float>(rand() % WIDTH);
-    y = static_cast<float>(rand() % HEIGHT);
-    speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
-    angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    float distance_x = 1.0f + static_cast<float>(rand() % 100) / 1.0f;
-    float distance_y = 1.0f + static_cast<float>(rand() % 100) / 1.0f;
-    Rectagle rectangle(x, y, speed, setColorCircle(), angle, distance_x, distance_y);
+        x = static_cast<float>(rand() % WIDTH);
+        y = static_cast<float>(rand() % HEIGHT);
+        speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
+        angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        float radius = 1.0f + static_cast<float>(rand() % 60) / 1.0f;
+        float outline = 1.0f + static_cast<float>(rand() % 40) / 10.0f;
+        Circle circle(x, y, speed, setColorCircle(), angle, radius, outline);
+        circles.push_back(circle);
 
-    x = static_cast<float>(rand() % WIDTH);
-    y = static_cast<float>(rand() % HEIGHT);
-    speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
-    angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    size = 1.0f + static_cast<float>(rand() % 100) / 1.0f;
-    Rhomb rhomb(x, y, speed, setColorCircle(), angle, size);
+        x = static_cast<float>(rand() % WIDTH);
+        y = static_cast<float>(rand() % HEIGHT);
+        speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
+        angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        radius = 1.0f + static_cast<float>(rand() % 100) / 1.0f;
+        Ellipse ellipse(x, y, speed, setColorCircle(), angle, radius);
+        ellipses.push_back(ellipse);
 
-    x = static_cast<float>(rand() % WIDTH);
-    y = static_cast<float>(rand() % HEIGHT);
-    speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
-    angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
-    float distance = 1.0f + static_cast<float>(rand() % 80) / 1.0f;
-    Triangle triangle(x, y, speed, setColorCircle(), angle, distance);
+        x = static_cast<float>(rand() % WIDTH);
+        y = static_cast<float>(rand() % HEIGHT);
+        speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
+        angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        float distance_x = 1.0f + static_cast<float>(rand() % 100) / 1.0f;
+        float distance_y = 1.0f + static_cast<float>(rand() % 100) / 1.0f;
+        Rectagle rectangle(x, y, speed, setColorCircle(), angle, distance_x, distance_y);
+        rectangles.push_back(rectangle);
 
+        x = static_cast<float>(rand() % WIDTH);
+        y = static_cast<float>(rand() % HEIGHT);
+        speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
+        angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        size = 1.0f + static_cast<float>(rand() % 100) / 1.0f;
+        Rhomb rhomb(x, y, speed, setColorCircle(), angle, size);
+        rhombs.push_back(rhomb);
+
+        x = static_cast<float>(rand() % WIDTH);
+        y = static_cast<float>(rand() % HEIGHT);
+        speed = 1.0f + static_cast<float>(rand() % 30) / 10.0f;
+        angle = static_cast<float>(rand() % 360) * 3.14159f / 180.0f;
+        float distance = 1.0f + static_cast<float>(rand() % 80) / 1.0f;
+        Triangle triangle(x, y, speed, setColorCircle(), angle, distance);
+        triangles.push_back(triangle);
+    }
     bool linearMode = true;
     cout << "Mode: Straight-line (SPACE bar for mode change)" << endl;
 
@@ -99,32 +117,34 @@ int main()
 
         window.clear(sf::Color::Black);
 
-        if (linearMode) {
-            circle.moveLinear(WIDTH, HEIGHT);
-            ellipse.moveLinear(WIDTH, HEIGHT);
-            rectangle.moveLinear(WIDTH, HEIGHT);
-            rhomb.moveLinear(WIDTH, HEIGHT);
-            line.moveLinear(WIDTH, HEIGHT);
-            point.moveLinear(WIDTH, HEIGHT);
-            triangle.moveLinear(WIDTH, HEIGHT);
-        }
-        else {
-            circle.moveChaotic(WIDTH, HEIGHT);
-            ellipse.moveChaotic(WIDTH, HEIGHT);
-            rectangle.moveChaotic(WIDTH, HEIGHT);
-            rhomb.moveChaotic(WIDTH, HEIGHT);
-            line.moveChaotic(WIDTH, HEIGHT);
-            point.moveChaotic(WIDTH, HEIGHT);
-            triangle.moveChaotic(WIDTH, HEIGHT);
-        }
-        circle.draw(window);
-        ellipse.draw(window);
-        rectangle.draw(window);
-        rhomb.draw(window);
-        line.draw(window);
-        point.draw(window);
-        triangle.draw(window);
+        for (int i = 0; i < 5; i++) {
+            if (linearMode) {
+                points[i].moveLinear(WIDTH, HEIGHT);
+                lines[i].moveLinear(WIDTH, HEIGHT);
+                circles[i].moveLinear(WIDTH, HEIGHT);
+                ellipses[i].moveLinear(WIDTH, HEIGHT);
+                rectangles[i].moveLinear(WIDTH, HEIGHT);
+                rhombs[i].moveLinear(WIDTH, HEIGHT);
+                triangles[i].moveLinear(WIDTH, HEIGHT);
+            }
+            else {
+                circles[i].moveChaotic(WIDTH, HEIGHT);
+                ellipses[i].moveChaotic(WIDTH, HEIGHT);
+                rectangles[i].moveChaotic(WIDTH, HEIGHT);
+                rhombs[i].moveChaotic(WIDTH, HEIGHT);
+                lines[i].moveChaotic(WIDTH, HEIGHT);
+                points[i].moveChaotic(WIDTH, HEIGHT);
+                triangles[i].moveChaotic(WIDTH, HEIGHT);
+            }
+            circles[i].draw(window);
+            ellipses[i].draw(window);
+            rectangles[i].draw(window);
+            rhombs[i].draw(window);
+            lines[i].draw(window);
+            points[i].draw(window);
+            triangles[i].draw(window);
 
+        }
         window.display();
     }
     
